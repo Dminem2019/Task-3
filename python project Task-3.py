8e55c1ed34962a6619f82b054d0bcda448a5292a
Dminem2019 <138094896+Dminem2019@users.noreply.github.com>
2023-12-16 16:36:01 +0530

    Update python project Task-3.py

--- python project Task-3.py
+++ python project Task-3.py
@@ -3,7 +3,7 @@ import random
 def guess_the_number():
     secret_number = random.randint(1, 100)
     attempts = 0
-
+ 
     print("Welcome to the Number Guessing Game!")
     print("I have selected a number between 1 and 100. Can you guess it?")
 
